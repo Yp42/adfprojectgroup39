bc6db997a04ede3d9dc80238d739572714277330
Aidan Kenning <aidan.kenning@mymtu.ie>
2025-12-02 13:09:41 +0000

    Update planet to use auto incremented IDs

--- src/main/java/com/appdev/project/entities/Planet.java
+++ src/main/java/com/appdev/project/entities/Planet.java
@@ -1,6 +1,8 @@
 package com.appdev.project.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,6 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Planet {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     int planetId;
     String name;
     String type;
